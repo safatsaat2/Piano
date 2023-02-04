9a98934a8ee3caa403d06024ec0d5f3ef8109a51
safatsaat2 <sakibsafat47@gmail.com>
2023-02-05 00:05:28 +0600

    piano is ok and fixed

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("tunes/a.wav"); 
+let allKeys = [],
+audio = new Audio("tunes/a.wav"); 
 
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150)
+
 }
 
 pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key);
     //  calling playTune function with passing data-key value as an argument
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
+const pressedKey = (e) => {
+   if(allKeys.includes(e.key)) playTune(e.key);
+}
 
 document.addEventListener("keydown", pressedKey);
