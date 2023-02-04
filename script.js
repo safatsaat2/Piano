c58170e75100b71ef0e447b2fba1208972c6da8b
safatsaat2 <sakibsafat47@gmail.com>
2023-02-04 23:33:28 +0600

    keys are set

---
+++ script.js
@@ -0,0 +1,16 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("tunes/a.wav"); 
+
+const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`;
+    audio.play();
+}
+
+pianoKeys.forEach(key => {
+    //  calling playTune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+
+document.addEventListener("keydown", pressedKey);
